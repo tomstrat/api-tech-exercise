7e4da505d13f73ddf3e31271df6169dd946d14bd
Tom <tomstratfic@hotmail.com>
2021-10-04 16:51:12 +0100

    add tests.js with testing for getAccessToken function

---
+++ src/tests.js
@@ -0,0 +1,19 @@
+//Sudo code for tests. Written before coding began
+
+//Test getAccessToken function
+//Requires parameters clientId, clientSecret
+//Returns access token
+getAccessToken(clientId, clientSecret)
+//Should return access token
+getAccessToken()
+//Should throw error lacking clientId and clientSecret
+getAccessToken(wrongId, wrongSecret)
+//Should throw error claiming incorrect credentials
+
+
+//Test retrieveTransactions function
+//Requires parameters userID, accessToken
+//Returns array of transactions
+
+
+//test formatTransactions function
