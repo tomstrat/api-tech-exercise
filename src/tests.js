dbba1a1186d7f89b4c86b4ae5f68d87ce4518986
Tom <tomstratfic@hotmail.com>
2021-10-04 16:55:07 +0100

    add tests for formatTransactions function

--- src/tests.js
+++ src/tests.js
@@ -14,6 +14,20 @@ getAccessToken(wrongId, wrongSecret)
 //Test retrieveTransactions function
 //Requires parameters userID, accessToken
 //Returns array of transactions
+retrieveTransactions(userID, accessToken)
+//should return array of transactions
+retrieveTransactions()
+//should throw error lacking userId and accessToken
+retrieveTransactions(wrongUserID, accessToken)
+//should throw error claiming uknown userID
 
 
 //test formatTransactions function
+//Requires parameters transactions
+//returns formatted array of transactions
+formatTransactions(transactions)
+//should return array of formatted transactions
+formatTransactions()
+//should throw error lacking transactions
+formatTransactions(emptyTransactions)
+//should return view for 0 transactions
