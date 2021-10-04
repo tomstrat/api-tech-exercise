55e64d4155ef80f2b1f01e892f4c86755df18fa4
Tom <tomstratfic@hotmail.com>
2021-10-04 17:03:22 +0100

    add getAccessToken Function

---
+++ src/transactionFunctions.js
@@ -0,0 +1,26 @@
+const got = require("got");
+
+module.exports = {
+  async getAccessToken(clientId, clientSecret){
+    //Create Auth for the request
+    const bufferAuth = new Buffer(`${clientId}:${clientSecret}`).toString("base64");
+
+    try{
+      const response = await got.post("https://obmockaspsp.moneyhub.co.uk/api/token", {
+        headers: {
+          Authorization: `Basic ${bufferAuth}`,
+        },
+        form:{
+          scope:"transactions", 
+          grant_type:"client_credentials"
+        }
+      });
+      const accessToken = JSON.parse(response.body).access_token;
+
+      return accessToken;
+      
+    } catch(e){
+      throw new Error("Incorrect Credentials");
+    }
+  },
+}
